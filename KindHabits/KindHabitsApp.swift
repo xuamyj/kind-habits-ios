7a328b8f8b15e914aaa6576a5bb0f10d04ab2fb8
Amy Xu <xuamyj@gmail.com>
2023-08-16 10:24:02 -0400

    Initial Commit

---
+++ KindHabits/KindHabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  KindHabitsApp.swift
+//  KindHabits
+//
+//  Created by Amy Xu on 8/16/23.
+//
+
+import SwiftUI
+
+@main
+struct KindHabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
