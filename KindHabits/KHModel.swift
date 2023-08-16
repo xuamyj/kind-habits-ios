94fbce2190b4d04dbf74ac7693b7098eab5822b7
Amy Xu <xuamyj@gmail.com>
2023-08-16 10:33:13 -0400

    Starting

---
+++ KindHabits/KHModel.swift
@@ -0,0 +1,12 @@
+//
+//  KHModel.swift
+//  KindHabits
+//
+//  Created by Amy Xu on 8/16/23.
+//
+
+import Foundation
+
+struct KHModel {
+    
+}
---
+++ KindHabits/KHViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  KHViewModel.swift
+//  KindHabits
+//
+//  Created by Amy Xu on 8/16/23.
+//
+
+import SwiftUI
+
+//class KHViewModel {
+//    private var model: KHModel
+//}
